4fcb953521c7d6218a5e926bd1eb98337597f9c3
Mark Raasveldt <mark.raasveldt@gmail.com>
2018-05-16 13:53:18 +0200

    Fix experiments for SQLite and MonetDBLite.

--- scripts/tpch/monetdblite/init.R
+++ scripts/tpch/monetdblite/init.R
@@ -6,12 +6,21 @@ library(readr)
 dbdir <- Sys.getenv('MONETDBLITE_DBDIR')
 con <- dbConnect(MonetDBLite::MonetDBLite(), dbdir)
 
-dbBegin(con)
-dbExecute(con, read_file('scripts/tpch/monetdb/schema.sql'))
-dbExecute(con, read_file('scripts/tpch/monetdb/load.sql.tmp'))
-dbExecute(con, read_file('scripts/tpch/monetdb/constraints.sql'))
-dbExecute(con, read_file('scripts/tpch/monetdb/analyze.sql'))
-dbCommit(con)
+run_statement <- function(statement) {
+	print(statement)
+	dbExecute(con, statement)
+}
+
+run_file <- function(fname) {
+	statements <- head(strsplit(read_file(fname), ';')[[1]], -1)
+	print(statements)
+	lapply(statements, run_statement)
+}
+
+run_file('scripts/tpch/monetdb/schema.sql')
+run_file('scripts/tpch/monetdb/load.sql.tmp')
+run_file('scripts/tpch/monetdb/constraints.sql')
+run_file('scripts/tpch/monetdb/analyze.sql')
 
 run_query <- function(i) {
   dbGetQuery(con, tpchr:::get_query(i))
--- scripts/tpch/sqlite/init.R
+++ scripts/tpch/sqlite/init.R
@@ -6,12 +6,22 @@ library(readr)
 dbdir <- Sys.getenv('SQLITE_DBDIR')
 con <- dbConnect(RSQLite::SQLite(), dbdir)
 
-dbBegin(con)
-dbExecute(con, read_file('scripts/tpch/sqlite/schema.sql'))
-dbExecute(con, read_file('scripts/tpch/sqlite/load.sql.tmp'))
-dbExecute(con, read_file('scripts/tpch/sqlite/constraints.sql'))
-dbExecute(con, read_file('scripts/tpch/sqlite/analyze.sql'))
-dbCommit(con)
+run_statement <- function(statement) {
+  print(statement)
+  dbExecute(con, statement)
+}
+
+run_file <- function(fname) {
+  statements <- head(strsplit(read_file(fname), ';')[[1]], -1)
+  print(statements)
+  lapply(statements, run_statement)
+}
+
+run_file('scripts/tpch/sqlite/schema.sql')
+run_file('scripts/tpch/sqlite/load.sql.tmp')
+run_file('scripts/tpch/sqlite/constraints.sql')
+run_file('scripts/tpch/sqlite/analyze.sql')
+
 
 run_query <- function(i) {
   dbGetQuery(con, tpchr:::get_query(i))

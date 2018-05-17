0c12ac8deecf87e3a6ab2b896ad850f89b5f5c36
Mark Raasveldt <mark.raasveldt@gmail.com>
2018-05-17 13:09:55 +0200

    Install devtools first.

--- scripts/packages.R
+++ scripts/packages.R
@@ -1,11 +1,12 @@
 
-packages = c("dplyr", "data.table", "dbplyr", "devtools", "MonetDBLite", "RPostgreSQL", "RMySQL", "RSQLite", "readr", "convey", "srvyr")
+packages = c("dplyr", "data.table", "dbplyr", "MonetDBLite", "RPostgreSQL", "RMySQL", "RSQLite", "readr", "convey", "srvyr")
 
 r = getOption("repos") # hard code the cloud repo for CRAN
 r["CRAN"] = "https://cloud.r-project.org"
 options(repos = r)
 rm(r)
 
+install.packages("devtools")
 devtools::install_github("ajdamico/lodown", dependencies = TRUE)
 install.packages(packages)
 devtools::install_github("hannesmuehleisen/tpchr")
